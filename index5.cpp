2ebc8ef3e05eeec5878d4497416d9352af66ef4d
unknown <computerengineering.edu.np@gmail.com>
2023-10-15 12:59:52 +0545

    prsnn

---
+++ index5.cpp
@@ -0,0 +1,36 @@
+
+#include <iostream>
+using namespace std;
+class automobile{
+          public:
+          char name[50];
+          char model[25];
+       void set()
+    {
+        cout<<" enter the name:";
+        cin>> name;
+        cout<<" enter the model number:";
+        cin>> model;
+    }
+};
+class car:public automobile
+{
+    
+    public:
+    void display()
+    {
+        cout<<" Brand name:"<<name<<endl;
+      
+        cout<<" Model number:"<<model;
+    }
+};
+
+int main()
+{
+    car c;
+    c.set();
+    c.display();
+    
+    return 0;
+
+}
